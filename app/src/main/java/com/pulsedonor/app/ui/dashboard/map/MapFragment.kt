425d0f47c0de82457564c0ec6c3043183d11c036
migena likaj <migena.likaj@studentet.uni-pr.edu>
2024-12-23 22:27:15 +0100

    Add: Implement map functionality and apply custom styling.

--- app/src/main/java/com/pulsedonor/app/ui/dashboard/map/MapFragment.kt
+++ app/src/main/java/com/pulsedonor/app/ui/dashboard/map/MapFragment.kt
@@ -1,10 +1,31 @@
 package com.pulsedonor.app.ui.dashboard.map
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
+import com.pulsedonor.app.R
 import com.pulsedonor.app.base.fragment.BaseFragment
 import com.pulsedonor.app.databinding.MapFragmentBinding
+import java.util.Locale
 
-class MapFragment : BaseFragment<MapFragmentBinding>() {
+class MapFragment : BaseFragment<MapFragmentBinding>(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+    private val TAG = MapFragment::class.java.simpleName
+    private val REQUEST_LOCATION_PERMISSION = 1
 
     override fun inflateBinding(layoutInflater: LayoutInflater): MapFragmentBinding =
         MapFragmentBinding.inflate(layoutInflater)
@@ -13,9 +34,122 @@ class MapFragment : BaseFragment<MapFragmentBinding>() {
     }
 
     override fun initViews() {
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     override fun onClicks() {
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        val latitude = 42.654237
+        val longitude = 21.162691
+        val zoomLevel = 15f
+        val overlaySize = 100f
+
+        val homeLatLng = LatLng(latitude, longitude)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+        map.addMarker(MarkerOptions().position(homeLatLng))
+
+        setMapLongClick(map)
+        setPoiClick(map)
+        setMapStyle(map)
+        enableMyLocation()
+    }
+
+    private fun setMapLongClick(map: GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+            // A Snippet is Additional text that's displayed below the title.
+            val snippet = String.format(
+                Locale.getDefault(),
+                "Lat: %1$.5f, Long: %2$.5f",
+                latLng.latitude,
+                latLng.longitude
+            )
+            map.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(getString(R.string.dropped_pin))
+                    .snippet(snippet)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+            )
+        }
+    }
+
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnMarkerClickListener { marker ->
+            Toast.makeText(
+                requireContext(),
+                "Clicked on marker",
+                Toast.LENGTH_SHORT
+            ).show()
+            false
+        }
+    }
+
+    private fun setMapStyle(map: GoogleMap) {
+        try {
+
+            val success = map.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    requireContext(),
+                    R.raw.map_style
+                )
+            )
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", e)
+        }
+    }
+
+    // Checks that users have given permission
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // Checks if users have given their location and sets location enabled if so.
+    private fun enableMyLocation() {
+        if (isPermissionGranted()) {
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            map.isMyLocationEnabled = true
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
+        }
+    }
 }
